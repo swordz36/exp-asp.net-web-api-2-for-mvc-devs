ac85c5463b6da339cfa92ec71d2f0deb94e097d5
Ertwyn Henry <ehenry@uritechnology.com>
2017-06-22 17:44:12 -0500

    Moving on to chapter 6

---
+++ Chapter 05 - SportsStore - Preparation/SportsStore/SportsStore/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private IRepository Repository { get; set; }
+
+        public ProductsController()
+        {
+            Repository = new ProductRepository();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return Repository.Products;
+        }
+
+        public Product GetProduct(int id)
+        {
+            return Repository.Products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public async Task PostProduct(Product product)
+        {
+            await Repository.SaveProductAsync(product);
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            await Repository.DeleteProductAsync(id);
+        }
+
+
+    }
+}
